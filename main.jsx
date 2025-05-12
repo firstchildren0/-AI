753fc8cc8719329ad800eceb8295da749700243e
firstchildren0 <koji8271@gmail.com>
2025-05-12 23:27:58 +0900

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DiagnosisQuizApp from "./quiz";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DiagnosisQuizApp />
+  </React.StrictMode>
+);
